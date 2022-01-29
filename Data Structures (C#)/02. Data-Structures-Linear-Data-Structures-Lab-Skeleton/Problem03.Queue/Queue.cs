23a5f9b64d1e1477105c69a054276bdb2fb43ce2
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-01-29 20:18:29 +0200

    Add custom Queue implementation.

--- Data Structures (C#)/02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/Problem03.Queue/Queue.cs
+++ Data Structures (C#)/02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/Problem03.Queue/Queue.cs
@@ -12,30 +12,94 @@
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var currentNode = this._head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                currentNode = currentNode.Next;
+            }
+
+            return false;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            this.IsEmptyQueue();
+
+
+            var topNodeValue = this._head.Value;
+
+            var newTopNode = this._head.Next;
+
+            this._head.Next = null;
+
+            this._head = newTopNode;
+
+            this.Count--;
+
+            return topNodeValue;
+
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            if (this._head == null)
+            {
+                this._head = new Node<T>()
+                {
+                    Value = item,
+                    Next = null
+                };
+            }
+            else
+            {
+                Node<T> toAdd = new Node<T>()
+                {
+                    Value = item
+                };
+
+                Node<T> current = this._head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = toAdd;
+            }
+
+            this.Count++;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.IsEmptyQueue();
+
+            var topNodeValue = this._head.Value;
+
+            return topNodeValue;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            while (this.Count > 0)
+            {
+                yield return this.Dequeue();
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-            => throw new NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void IsEmptyQueue()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException(nameof(Count));
+            }
+        }
     }
 }
