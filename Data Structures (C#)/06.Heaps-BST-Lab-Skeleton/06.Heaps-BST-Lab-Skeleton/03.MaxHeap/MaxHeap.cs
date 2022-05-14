0e2b91d9e7ff0c9c6ad79a02fb241209bf1939ad
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-05-14 16:29:14 +0300

    Implemented second and third exercise

--- Data Structures (C#)/06.Heaps-BST-Lab-Skeleton/06.Heaps-BST-Lab-Skeleton/03.MaxHeap/MaxHeap.cs
+++ Data Structures (C#)/06.Heaps-BST-Lab-Skeleton/06.Heaps-BST-Lab-Skeleton/03.MaxHeap/MaxHeap.cs
@@ -53,20 +53,57 @@
 
         public T ExtractMax()
         {
-            if (this.elements.Count > 0)
+            if (this.elements.Count == 0)
             {
                 throw new InvalidOperationException();
             }
 
             T element = this.elements[0];
+
             this.Swap(0,this.elements.Count - 1);
+
             this.elements.RemoveAt(this.elements.Count - 1);
+
             this.HeapifyDown(0);
+
+            return element;
         }
 
         private void HeapifyDown(int index)
         {
-            throw new NotImplementedException();
+            var biggerChildIndex = this.GetBiggerChildIndex(index);
+
+            while (biggerChildIndex >= 0 && biggerChildIndex < this.elements.Count && this.IsGreater(biggerChildIndex, index))
+            {
+                this.Swap(biggerChildIndex, index);
+
+                index = biggerChildIndex;
+                biggerChildIndex = this.GetBiggerChildIndex(index);
+            }
+        }
+
+        private int GetBiggerChildIndex(int index)
+        {
+            var firstChildIndex = index * 2 + 1;
+            var secondChildIndex = index * 2 + 2;
+
+            if (secondChildIndex < this.elements.Count)
+            {
+                if (this.IsGreater(firstChildIndex, secondChildIndex))
+                {
+                    return firstChildIndex;
+                }
+
+                return secondChildIndex;
+            }
+            else if (firstChildIndex  < this.elements.Count)
+            {
+                return firstChildIndex;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         public T Peek()
