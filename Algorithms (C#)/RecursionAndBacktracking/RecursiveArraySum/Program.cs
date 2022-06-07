87dc173b19649503fc5638b3f4e7e79dfce7eb2b
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-07 14:41:13 +0300

    Exercise 1. Recursive Array Sum

---
+++ Algorithms (C#)/RecursionAndBacktracking/RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()?
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(GetSum(numbers));
+        }
+
+        public static int GetSum(int[] numbers, int index = 0)
+        {
+            if (numbers.Length <= index)
+            {
+                return 0;
+            }
+
+            return numbers[index] + GetSum(numbers, index + 1);
+        }
+    }
+}
