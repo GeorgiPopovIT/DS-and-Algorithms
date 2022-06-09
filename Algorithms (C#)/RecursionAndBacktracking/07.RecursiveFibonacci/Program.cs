d8b94b042e2e57494ab471cddf8c460017c684e4
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-09 13:30:59 +0300

    Commit 07. Recursive Fibonacci exercise.

---
+++ Algorithms (C#)/RecursionAndBacktracking/07.RecursiveFibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07.RecursiveFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFibonacci(n));
+        }
+
+        private static long GetFibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
