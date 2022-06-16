f45ba25f7bf6b4824a40245bfcd918645e970e92
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-16 15:32:52 +0300

    Commit 05. combination without reps

---
+++ Algorithms (C#)/Combinatorial_Probelms/05. Combinations without Repetition/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05._Combinations_without_Repetition
+{
+    internal class Program
+    {
+        private static int k;
+        private static string[] input;
+        private static string[] combinations;
+
+
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            CombinationWithoutReps(0,0);
+        }
+
+        private static void CombinationWithoutReps(int index,int elementStartIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(String.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementStartIndex; i < input.Length; i++)
+            {
+                combinations[index] = input[i];
+                CombinationWithoutReps(index + 1,i + 1);
+            }
+        }
+    }
+}
