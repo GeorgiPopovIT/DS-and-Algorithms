997f572ddb65a8d14aea05fe9749b343249b7f4d
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-16 17:32:43 +0300

    Commit 07. N Choose K Count

---
+++ Algorithms (C#)/Combinatorial_Probelms/07. N Choose K Count/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._N_Choose_K_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(NChooseKCount(n, k));
+        }
+
+        private static int NChooseKCount(int n, int k)
+        {
+            if (k == 0)
+            {
+                return 1;
+
+            }
+            return (n * NChooseKCount(n - 1, k - 1)) / k;
+        }
+    }
+}
