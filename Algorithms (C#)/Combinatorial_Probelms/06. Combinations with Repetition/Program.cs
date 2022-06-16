ccfded6140763645ed815e162734007a4b56f7a2
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-16 15:39:19 +0300

    Commit 06. Combiantion with reps

---
+++ Algorithms (C#)/Combinatorial_Probelms/06. Combinations with Repetition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06._Combinations_with_Repetition
+{
+    internal class Program
+    {
+        private static int k;
+        private static string[] input;
+        private static string[] combinations;
+
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+
+            CombinationWithReps(0, 0);
+        }
+        private static void CombinationWithReps(int index, int elementStartIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(String.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementStartIndex; i < input.Length; i++)
+            {
+                combinations[index] = input[i];
+                CombinationWithReps(index + 1, i);
+            }
+        }
+    }
+}
