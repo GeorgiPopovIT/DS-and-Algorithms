c52d83fced5bb9de7afdb3d3f139234ac68bd7b6
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-17 17:51:43 +0300

    Commit 01. Reverse Array Recursively.

---
+++ Algorithms (C#)/RecursionAndCombinatorialProblems-Exercises/01.ReversedArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01.ReverseArray
+{
+    internal class Program
+    {
+        private static string[] input;
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine().Split();
+            var lastIndex = input.Length - 1;
+
+            ReverseArray(0, lastIndex);
+        }
+
+        private static void ReverseArray(int startIndex, int lastIndex)
+        {
+            if (startIndex >= lastIndex)
+            {
+                Console.WriteLine(string.Join(" ", input));
+                return;
+            }
+
+            Swap(startIndex, lastIndex);
+
+            ReverseArray(startIndex + 1, lastIndex - 1);
+        }
+
+        private static void Swap(int firstSwap, int secondSwap)
+        {
+            var temp = input[firstSwap];
+            input[firstSwap] = input[secondSwap];
+            input[secondSwap] = temp;
+        }
+    }
+}
