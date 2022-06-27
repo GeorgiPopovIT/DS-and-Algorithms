da127ca0f38a74eaebfe8bb1623832739a30ac3c
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-27 15:21:08 +0300

    Implemented methods AddOrReplace, indexer, Keys, Values

--- Data Structures (C#)/09. Data-Structures-Hash-Tables-Sets-and-Dictionaries-Lab-CSharp-Skeleton/HashTable/HashTable.cs
+++ Data Structures (C#)/09. Data-Structures-Hash-Tables-Sets-and-Dictionaries-Lab-CSharp-Skeleton/HashTable/HashTable.cs
@@ -8,6 +8,8 @@ public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
     private LinkedList<KeyValue<TKey, TValue>>[] slots;
 
     public const int InitialCapacity = 16;
+    public const float LoadFactor = 0.75f;
+
     public int Count { get; private set; }
 
     public int Capacity
@@ -53,7 +55,25 @@ public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
 
     public bool AddOrReplace(TKey key, TValue value)
     {
-        throw new NotImplementedException();
+        GrowIfNeeded();
+        int slotNumber = this.FindSlotNumber(key);
+        if (this.slots[slotNumber] == null)
+        {
+            this.slots[slotNumber] = new LinkedList<KeyValue<TKey, TValue>>();
+        }
+        foreach (var element in this.slots[slotNumber])
+        {
+            if (element.Key.Equals(key))
+            {
+                element.Value = value;
+                return false;
+            }
+        }
+
+        var newElement = new KeyValue<TKey, TValue>(key, value);
+        this.slots[slotNumber].AddLast(newElement);
+        this.Count++;
+        return true;
     }
 
     public TValue Get(TKey key)
@@ -70,12 +90,17 @@ public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
     {
         get
         {
-            throw new NotImplementedException();
-            // Note: throw an exception on missing key
+            var element = this.Get(key);
+            if (element == null)
+            {
+                throw new Exception("Element is not found by this key.");
+            }
+            return element;
         }
         set
         {
-            throw new NotImplementedException();
+            var element = this.Get(key);
+            this.AddOrReplace(key,element);
         }
     }
 
@@ -117,7 +142,23 @@ public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
 
     public bool Remove(TKey key)
     {
-        throw new NotImplementedException();
+        int slotNumber = this.FindSlotNumber(key);
+        var elements = this.slots[slotNumber];
+        if (elements != null)
+        {
+            var currentElement = elements.First;
+            while (currentElement != null)
+            {
+                if (currentElement.Value.Key.Equals(key))
+                {
+                    elements.Remove(currentElement);
+                    this.Count--;
+                    return true;
+                }
+                currentElement = currentElement.Next;
+            }
+        }
+        return false;
     }
 
     public void Clear()
@@ -138,7 +179,7 @@ public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
     {
         get
         {
-            return null;
+            return this.Select(element => element.Value);
         }
     }
 
@@ -173,6 +214,21 @@ public class HashTable<TKey, TValue> : IEnumerable<KeyValue<TKey, TValue>>
 
         //    //this.slots = newArray;
         //}
+        if ((float)(this.Count + 1) / this.Capacity > LoadFactor)
+        {
+            this.Grow();
+        }
+    }
+
+    private void Grow()
+    {
+        var newHashTable = new HashTable<TKey, TValue>(2 * this.Capacity);
+        foreach (var element in this)
+        {
+            newHashTable.Add(element.Key, element.Value);
+        }
+        this.slots = newHashTable.slots;
+        this.Count = newHashTable.Count;
     }
 
     private int FindSlotNumber(TKey key)
