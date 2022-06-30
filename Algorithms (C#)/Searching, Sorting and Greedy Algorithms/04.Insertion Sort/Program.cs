de87736fd245c43439088d9ee48b9cf75ef3601f
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-30 14:16:08 +0300

    Implemented Insertion Sort.

---
+++ Algorithms (C#)/Searching, Sorting and Greedy Algorithms/04.Insertion Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Insertion_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            InsertionSort(input);
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+
+        private static void InsertionSort(int[] input)
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && input[j - 1] > input[j])
+                {
+                    Swap(input, j, j - 1);
+                    j--;
+                }
+            }
+        }
+
+        private static void Swap(int[] input, int first, int second)
+        {
+            var temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
+        }
+    }
+}
