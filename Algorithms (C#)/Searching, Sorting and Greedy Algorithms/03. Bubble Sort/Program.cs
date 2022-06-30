c2d43d7eef8b978eabecaaa12b5967c4ecf17f11
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-30 11:02:40 +0300

    Bubble Sort

---
+++ Algorithms (C#)/Searching, Sorting and Greedy Algorithms/03. Bubble Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Bubble_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            BubbleSort(input);
+
+            Console.WriteLine(string.Join("",input));
+        }
+
+        private static void BubbleSort(int[] input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        Swap(input, j, j + 1);
+                    }
+                }
+            }
+        }
+        private static void Swap(int[] input, int first, int second)
+        {
+            var temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
+        }
+    }
+}
