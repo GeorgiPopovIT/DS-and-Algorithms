176a2f08dcf8260c8a397157f036f46d863e1332
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-30 10:10:11 +0300

    Implemented 02.Selection Sort.

--- Algorithms (C#)/Searching, Sorting and Greedy Algorithms/02. Selection Sort/Program.cs
+++ Algorithms (C#)/Searching, Sorting and Greedy Algorithms/02. Selection Sort/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace _02._Selection_Sort
+﻿using System;
+using System.Linq;
+
+namespace _02._Selection_Sort
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+
+            SelectionSort(input);
+
+            Console.WriteLine(String.Join(" ", input));
+        }
+
+        private static void SelectionSort(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                var min = i;
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[j] < input[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                Swap(input, i, min);
+            }
+        }
+
+        private static void Swap(int[] input, int first, int second)
+        {
+            var temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
         }
     }
 }
