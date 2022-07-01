3b976659f7e4f73ad155b4040315363e63938653
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-07-01 16:31:39 +0300

    Create MergeSort project

---
+++ Algorithms (C#)/Searching, Sorting and Greedy Algorithms/06. Merge Sort/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _06._Merge_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+
+            var sorted = MergeSort(input);
+
+            Console.WriteLine(string.Join(" ", sorted));
+        }
+
+        private static int[] MergeSort(int[] input)
+        {
+            if (input.Length <=1)
+            {
+
+            }
+        }
+
+        private static void Swap(int[] input, int first, int second)
+        {
+            var temp = input[first];
+            input[first] = input[second];
+            input[second] = temp;
+        }
+    }
+}
