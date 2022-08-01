020d3b92d940ca7a8b0ec19aa1d6831bcea593f5
GeorgiPopovIT <gogopopo262003@gmail.com>
2022-08-01 13:02:28 +0300

    Commit new exercise

---
+++ Algorithms (C#)/Introduction to Dynamic programming/01. Binomial Coefficients/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Binomial_Coefficients
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Bimomial(n, k));
+        }
+
+        public static long Bimomial(long n, long k)
+        {
+            double sum = 0;
+            for (long i = 0; i < k; i++)
+            {
+                sum += Math.Log10(n - i);
+                sum -= Math.Log10(i + 1);
+            }
+            return (long)Math.Pow(10, sum);
+        }
+    }
+}
